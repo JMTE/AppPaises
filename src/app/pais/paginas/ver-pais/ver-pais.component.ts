9bf6193f956062f8d2eb58fa6eb4813d3c42fcfa
JMTE <JMTEdeveloper@outlook.es>
2021-08-13 13:48:00 +0200

    Probamos la funcionalidad de switchmap para capturar el codigo del pais

--- src/app/pais/paginas/ver-pais/ver-pais.component.ts
+++ src/app/pais/paginas/ver-pais/ver-pais.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
 import { PaisService } from '../../servicios/pais.service';
 
 @Component({
@@ -15,24 +16,40 @@ export class VerPAisComponent implements OnInit {
 
   ngOnInit(): void {
 
-    this.activatedRoute.params.subscribe(({id})=>{
+    // this.activatedRoute.params.subscribe(({id})=>{
 
-      //Aqui atrapamos el id del pais
+    //   //Aqui atrapamos el id del pais
 
-      console.log(id);
+    //   console.log(id);
 
-      //A continuacion tenemos que hacer otro observable para traer la informacion del pais 
-      //Podemos crear otro servicio nuevo o hacer otra funcion en uno de nuestros servicios
-      //Esta vez vamos a hacer lo segundo
+    //   //A continuacion tenemos que hacer otro observable para traer la informacion del pais 
+    //   //Podemos crear otro servicio nuevo o hacer otra funcion en uno de nuestros servicios
+    //   //Esta vez vamos a hacer lo segundo
 
-      this.PaisService.getPaisCodigo(id).subscribe(pais=>{
+    //   this.PaisService.getPaisCodigo(id).subscribe(pais=>{
 
-        console.log(pais);
+    //     console.log(pais);
         
-      })
+    //   })
 
 
       
+    // })
+
+
+    //A CONTINUACION VAMOS A HACER LO MISMO PERO TRABAJANDO CON RXJS Y SWITCHMAP
+
+    this.activatedRoute.params
+    .pipe(
+      //El switchmap nos permite recibir un observable y devolver otro observable
+      switchMap((param)=>this.PaisService.getPaisCodigo(param.id))
+    )
+    
+    .subscribe(respuesta=>{
+
+      console.log(respuesta);
+      
+
     })
 
   }
