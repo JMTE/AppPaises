0e323ff57e0dc4e69a28896830d8bebf59a0ebd1
JMTE <JMTEdeveloper@outlook.es>
2021-08-16 12:26:51 +0200

    Se empieza a implementar el apartado de busqueda por continente. Para ello creamos una funcion para seleccionar por continente con botones.

--- src/app/pais/paginas/por-continente/por-continente.component.ts
+++ src/app/pais/paginas/por-continente/por-continente.component.ts
@@ -1,16 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-por-continente',
   templateUrl: './por-continente.component.html',
-  styles: [
+  styles: [`
+
+button{
+  margin-right:1.5px
+
+  
+}
+  `
   ]
 })
-export class PorContinenteComponent implements OnInit {
+export class PorContinenteComponent{
+
+  regiones:string[]= ['africa', 'america', 'asia', 'europe', 'oceania'];
+
+  regionActiva:string="";
+
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
 
+  activarRegion(region:string){
+
+    this.regionActiva=region;
+
+    //Hacer el llamado al servicio para traer los paises por esa region
+
+
+  }
+  
 }
