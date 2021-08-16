8d58af6e4becc166784529504ec76c9eafb5847e
JMTE <JMTEdeveloper@outlook.es>
2021-08-16 13:24:30 +0200

    Para que cuando se descargue la informacion de los paises que seleccionamos no consuma mas recursos de los necesarios, hacemos una seleccion de las caracteristicas que nos interesa informar y solamente le indicamos mediante filtros que descargue esa informacion, de esta manera el archivo descargado es mas liviano y la operacion se realiza mas rapida

--- src/app/pais/servicios/pais.service.ts
+++ src/app/pais/servicios/pais.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Country } from '../interfaces/pais.interfaces';
@@ -10,6 +10,11 @@ export class PaisService {
 
   private apiUrl:string="https://restcountries.eu/rest/v2"
 
+  get httpParams(){
+
+    return new HttpParams().set('fields','name;capital;alpha2Code;flag;population' )
+  }
+
   //Inyectamos el servicio httpClient, recordemos importarlo en los modulos
   constructor( private http:HttpClient) { }
 
@@ -17,7 +22,7 @@ export class PaisService {
 
 
     const url= `${this.apiUrl}/name/${termino}`
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url, {params:this.httpParams});
   }
 
   getPaisCodigo(id: string):Observable<Country>{
@@ -26,15 +31,17 @@ export class PaisService {
     //El codigo del pais nos lo devuelve como alpha y solo nos retorna un pais, no un array
 
     const url= `${this.apiUrl}/alpha/${id}`
-    return this.http.get<Country>(url);
+    return this.http.get<Country>(url, {params:this.httpParams});
   }
 
   buscarPaisesContinente(continente:string):Observable<Country[]>{
 
     //El codigo del pais nos lo devuelve como region y nos retorna un array de paises
 
+    
+
     const url= `${this.apiUrl}/region/${continente}`
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url, {params:this.httpParams});
 
 
   }
